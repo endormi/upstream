ef1a6c8539284f8297953e0fdb00abc186e2f024
endormi <ernosalo14@gmail.com>
2022-06-25 13:45:03 +0300

    Check for Git and jq

--- upstream.sh
+++ upstream.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Check if Git is installed (just in case)
+if [[ "$(which git)" == "" ]]; then
+  echo "You need to install Git and setup the Git Configs."
+  echo -e "Follow this tutorial: \e]8;;https://git-scm.com/book/en/v2/Getting-Started-First-Time-Git-Setup\aFirst-time Git Setup\e]8;;\a."
+  exit
+fi
+
+# Check if jq is installed
+if [[ "$(which jq)" == "" ]]; then
+  echo "You need to install jq."
+  exit
+fi
+
 function list_repo_info() {
   arg1=$1
   arg2=$2
