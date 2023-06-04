343d2678b4dd46f39712a0f141a06cd2c554236d
endormi <ernosalo14@gmail.com>
2023-06-04 17:04:23 +0300

    Add ACCESS_TOKEN check and optimize code

--- upstream.sh
+++ upstream.sh
@@ -2,25 +2,26 @@
 
 ACCESS_TOKEN=""
 
+if [[ -z "$ACCESS_TOKEN" ]]; then
+  echo "No ACCESS_TOKEN provided."
+  exit
+fi
+
 # Check if Git is installed (just in case)
-if [[ -z "$(which git)" ]]; then
+if ! command -v git &>/dev/null; then
   echo "You need to install Git and setup Git Configs."
   echo -e "Follow this tutorial: \e]8;;https://git-scm.com/book/en/v2/Getting-Started-First-Time-Git-Setup\aFirst-time Git Setup\e]8;;\a."
   exit
 fi
 
 # Check if jq is installed
-[[ -z "$(which jq)" ]] && echo "You need to install jq." && exit
+if ! command -v jq &>/dev/null; then echo "You need to install jq."; exit; fi
 
 function list_repo_info() {
   arg1=$1
   arg2=$2
 
-  if [[ $arg2 == "local" ]]; then
-    arg2="--local"
-  else
-    arg2=""
-  fi
+  [[ $arg2 == "local" ]] && arg2="--local" || arg2=""
 
   if [[ `git config --list $arg2 | grep $arg1 | wc -l` -eq 0 ]]; then
     echo "$arg1 not found!"
